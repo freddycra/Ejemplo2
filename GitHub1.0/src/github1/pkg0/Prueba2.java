c903f4607ec90e38424d33c16545fa1438d747de
freddycra <freddycra@latitude-e6400>
2017-10-27 18:15:51 -0600

    Es la primera parte del ejemplo 2

---
+++ GitHub1.0/src/github1/pkg0/Prueba2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github1.pkg0;
+
+/**
+ *
+ * @author freddycra
+ */
+public class Prueba2 {
+
+    public Prueba2() {
+        var = 5;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(var);
+    }
+
+    private int var;
+}
